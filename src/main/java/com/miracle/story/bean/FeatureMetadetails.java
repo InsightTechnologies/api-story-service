c90db10b761873d22ccd7120d298949fdbc0a5fe
MaheshMediboyina <MaheshMediboyina@users.noreply.github.com>
2019-05-18 01:34:14 +0530

    Updated logic

---
+++ src/main/java/com/miracle/story/bean/FeatureMetadetails.java
@@ -0,0 +1,27 @@
+package com.miracle.story.bean;
+
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+@Component
+@JsonPropertyOrder({"projectName","id"})
+public class FeatureMetadetails {
+	@JsonProperty(required = true)
+	private String projectName;
+	@JsonProperty(required = true)
+	private int id;
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}	
+}
--- src/main/java/com/miracle/story/exception/StoryErrorCode.java
+++ src/main/java/com/miracle/story/exception/StoryErrorCode.java
@@ -1,5 +1,7 @@
 package com.miracle.story.exception;
 
-public class StoryErrorCode {
-
+public interface StoryErrorCode {
+	String INVALID_FEATURE_ID = "MSB.STR.INF.000";
+	String INVALID_PROJECT_NAME = "MSB.STR.INP.000";
+	String STORY_CONTROLLER_UNKNOWN_EXCEPTION = "MSB.STR.UN.000";
 }
