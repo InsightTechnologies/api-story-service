c1f4ca37a6f990436b4079e77d8d40c5bc240818
samhithar <samhithaeega@gmail.com>
2019-05-31 15:36:59 -0500

    Updated commit

---
+++ src/main/java/com/miracle/story/controller/StoryControllerErrorHandler.java
@@ -0,0 +1,32 @@
+package com.miracle.story.controller;
+
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.miracle.exception.APIExceptionResponse;
+import com.miracle.exception.GatewayServiceException;
+
+@ControllerAdvice(basePackages = { "com.miracle.story" })
+public class StoryControllerErrorHandler {
+	@Autowired
+	private APIExceptionResponse exceptionMessage;
+
+	@ExceptionHandler({ Exception.class })
+	public ResponseEntity<APIExceptionResponse> handle(Exception exception, HttpServletRequest httpServletRequest) {
+		exceptionMessage.setErrorCode(((GatewayServiceException) exception).getErrorCode(exception, exceptionMessage));
+		exceptionMessage.setErrorDescription(exception.getLocalizedMessage());
+		exceptionMessage.setTimestamp(Instant.now());
+		exceptionMessage.setStackTraceElement(exception.getStackTrace()[0]);
+		exceptionMessage.setCompleteTrace(Arrays.toString(exception.getStackTrace()));
+		exceptionMessage.setUri(httpServletRequest.getRequestURL());
+		return new ResponseEntity<APIExceptionResponse>(exceptionMessage, HttpStatus.BAD_REQUEST);
+	}
+}
--- src/main/java/com/miracle/story/exception/StoryErrorCode.java
+++ src/main/java/com/miracle/story/exception/StoryErrorCode.java
@@ -1,5 +1,7 @@
 package com.miracle.story.exception;
 
-public class StoryErrorCode {
-
+public final class StoryErrorCode {
+	public static final String INVALID_FEATURE_ID = "MSB.STR.INF.000";
+	public static final String INVALID_PROJECT_NAME = "MSB.STR.INP.000";
+	public static final String STORY_CONTROLLER_UNKNOWN_EXCEPTION = "MSB.STR.UN.000";
 }
